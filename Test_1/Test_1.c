2d9494559a57425c2cc46d506f065dd1f6196004
SnowK Die <1752351098@qq.com>
2022-12-06 15:40:46 +0800

    推新

--- Test_1/Test_1.c
+++ Test_1/Test_1.c
@@ -312,8 +312,80 @@ int main()
 			(2)猜错了，会告诉大了还是小了，继续猜
 		3.游戏一直进行，除非退出游戏*/
 
-int main()
-{
-	
-	return 0;
-}
+//#include <stdlib.h>
+//#include <time.h>
+//
+//void menu()
+//{
+//
+//	printf("***********************************\n");
+//	printf("************  1.Play  *************\n");
+//	printf("************  0.exit  *************\n");
+//	printf("***********************************\n");
+//
+//}
+//
+//void game()
+//{
+//	//先生成随机数，以时间做种
+//	int num = rand() % 100 + 1;//%100的余数是0-99，然后+1即可达成范围为1-100
+//
+//	//先做框架
+//	int guess = 0;
+//	while (1)
+//	{
+//		printf("请猜数字:>");
+//		scanf("%d", &guess);
+//		if (guess > num)
+//		{
+//			printf("猜大啦\n");
+//		}
+//		else if (guess < num)
+//		{
+//			printf("猜小啦\n");
+//		}
+//		else
+//		{
+//			printf("恭喜你，猜对了，正确答案为%d\n", num);
+//			break;
+//		}
+//	}
+//
+//
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	//先调用srand() 掷出rand()所需要的随机数种子
+//	//该种子一个程序中，有一次生成即可
+//	//所以放在主函数开头中
+//	srand((unsigned int)time(NULL));
+//
+//	do 
+//	{
+//		//开始游戏先打印菜单
+//		menu();
+//		printf("请选择:>");
+//		//选择游戏模式
+//		scanf("%d", &input);
+//
+//		//判断游戏是否进行（即状态）
+//		//switch 决定游戏框架
+//		//game()决定游戏如何游玩
+//		switch (input)
+//		{
+//		case 1:
+//			game();
+//			break;
+//		case 0:
+//			printf("退出游戏\n");
+//			break;
+//		default:
+//			printf("选择错误，请重新选择\n");
+//			break;
+//		}
+//	} while (input);
+//	
+//	return 0;
+//}
