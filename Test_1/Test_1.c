4296320d681091465a45847cabef6a02c0850a77
DieSnowK <1752351098@qq.com>
2023-06-23 00:02:10 +0800

    2023.6.23

--- Test_1/Test_1.c
+++ Test_1/Test_1.c
@@ -3314,7 +3314,7 @@ int main()
 //	printf("%d\n", FindMissingNum(arr, sz));
 //	return 0;
 //}
- 
+
 //思路3：排序 + 二分查找  冒泡O(N^2)  快排O(N*logN) - 效率相对较低
 //int main()
 //{
@@ -3847,3 +3847,184 @@ int main()
 //
 //	return slow;
 //}
+
+//合并两个有序链表
+//将两个升序链表合并为一个新的升序链表并返回
+//新链表是通过拼接给定的两个链表的所有节点组成的
+//struct ListNode 
+//{
+//	int val;
+//	struct ListNode* next;
+//};
+//
+////方法：归并  --  从头比较，取小的尾插的新链表
+////版本一：不带哨兵位的头节点
+//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
+//{
+//	if (list1 == NULL)
+//	{
+//		return list2;
+//	}
+//	if (list2 == NULL)
+//	{
+//		return list1;
+//	}
+//
+//	struct ListNode* tail = NULL;
+//	struct ListNode* head = NULL;
+//
+//	while (list1 && list2)
+//	{
+//		if (list1->val > list2->val)
+//		{
+//			if (tail == NULL)
+//			{
+//				head = tail = list2;
+//			}
+//			else
+//			{
+//				tail->next = list2;
+//				tail = tail->next;
+//			}
+//
+//			list2 = list2->next;
+//		}
+//		else
+//		{
+//			if (tail == NULL)
+//			{
+//				head = tail = list1;
+//			}
+//			else
+//			{
+//				tail->next = list1;
+//				tail = tail->next;
+//			}
+//
+//			list1 = list1->next;
+//		}
+//	}
+//
+//	if (list1)
+//	{
+//		tail->next = list1;
+//	}
+//
+//	if (list2)
+//	{
+//		tail->next = list2;
+//	}
+//
+//	return head;
+//}
+//
+////版本二：带哨兵位的头节点
+////这个例子可以深切体会到哨兵位头节点的好处
+//struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2)
+//{
+//	struct ListNode* tail, * head;
+//	head = tail = (struct ListNode*)malloc(sizeof(struct ListNode));
+//	head->next = NULL;
+//
+//	while (list1 && list2)  //有一个为空，则结束
+//	{
+//		if (list1->val > list2->val)
+//		{
+//			tail->next = list2;
+//			tail = tail->next;
+//
+//			list2 = list2->next;
+//		}
+//		else
+//		{
+//			tail->next = list1;
+//			tail = tail->next;
+//
+//			list1 = list1->next;
+//		}
+//	}
+//
+//	if (list1)
+//	{
+//		tail->next = list1;
+//	}
+//
+//	if (list2)
+//	{
+//		tail->next = list2;
+//	}
+//
+//	struct ListNode* list = head->next;
+//	free(head);
+//	return list;
+//}
+
+//链表分割
+//有一链表的头指针 ListNode* pHead，给一定值x
+//编写一段代码将所有小于x的结点排在其余结点之前，且不能改变原来的数据顺序，返回重新排列后的链表的头指针
+//struct ListNode
+//{
+//	int val;
+//	struct ListNode* next;
+//};
+//
+//struct ListNode* partition(struct ListNode* pHead, int x)
+//{
+//	struct ListNode* greaterhead, * greatertail, * lesshead, * lesstail;
+//	//带哨兵位头节点
+//	greaterhead = greatertail = (struct ListNode*)malloc(sizeof(struct ListNode));
+//	lesshead = lesstail = (struct ListNode*)malloc(sizeof(struct ListNode));
+//	greatertail->next = NULL;
+//	lesstail->next = NULL;
+//
+//	struct ListNode* cur = pHead;
+//	while (cur)
+//	{
+//		if (cur->val < x)
+//		{
+//			//比x小的尾插入到第一个链表
+//			lesstail->next = cur;
+//			lesstail = lesstail->next;
+//		}
+//		else
+//		{
+//			//比x大的尾插入第二个链表
+//			greatertail->next = cur;
+//			greatertail = greatertail->next;
+//		}
+//
+//		cur = cur->next;
+//	}
+//	//相连临界头尾
+//	lesstail->next = greaterhead->next;
+//	greatertail->next = NULL;  //将尾节点置空
+//
+//	struct ListNode* head = lesshead->next;
+//	free(lesshead);
+//	free(greaterhead);
+//
+//	return head;
+//}
+
+//回文链表
+//判断链表是否为回文结构  --  中心对称
+//要求：时间复杂度：O(N)  空间复杂度：O(1)
+//思路：找中间节点，再逆置，再比较
+struct ListNode 
+{
+	int val;
+	struct ListNode* next;
+};
+
+
+//相交链表
+//给你两个单链表的头节点 headA 和 headB
+//找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 
+//struct ListNode 
+//{
+//	int val;
+//	struct ListNode* next;
+//};
+
+//环形链表
+//给你一个链表的头节点 head ，判断链表中是否有环
