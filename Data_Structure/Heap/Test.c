11dd4c14516951e07a172d2fc46ca7f7310c5f51
DieSnowK <1752351098@qq.com>
2023-07-21 21:39:33 +0800

    2023.7.21

--- Data_Structure/Heap/Test.c
+++ Data_Structure/Heap/Test.c
@@ -25,25 +25,25 @@ void Test()
 //1.你得先写一个Heap的数据结构，反而复杂
 //2.有O(N)的空间复杂度
 //所以不太好
-void HeapSort(HPDataType* a, int size)
-{
-	Heap hp;
-	HeapInit(&hp);
-
-	for (int i = 0; i < size; i++)
-	{
-		HeapPush(&hp, a[i]);
-	}
-
-	int i = 0;
-	while (!isHeapEmpty(&hp))
-	{
-		a[i++] = HeapTop(&hp);
-		HeapPop(&hp);
-	}
-
-	HeapDestroy(&hp);
-}
+//void HeapSort(HPDataType* a, int size)
+//{
+//	Heap hp;
+//	HeapInit(&hp);
+//
+//	for (int i = 0; i < size; i++)
+//	{
+//		HeapPush(&hp, a[i]);
+//	}
+//
+//	int i = 0;
+//	while (!isHeapEmpty(&hp))
+//	{
+//		a[i++] = HeapTop(&hp);
+//		HeapPop(&hp);
+//	}
+//
+//	HeapDestroy(&hp);
+//}
 
 //升序 -- 建大堆
 //降序 -- 建小堆
@@ -73,29 +73,96 @@ void Heapsort(HPDataType* a, int size)
 	}
 }
 
-void TestHeapSort()  //堆排序
+//void TestHeapSort()  //堆排序
+//{
+//	int arr[] = { 27,15,19,18,28,34,65,49,25,37 };
+//
+//	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//
+//	Heapsort(arr, sizeof(arr) / sizeof(arr[0]));
+//
+//	for(int i = 0;i<sizeof(arr) / sizeof(arr[0]); i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	printf("\n");
+//}
+
+
+void PrintTopK(int* a, int n, int k)
 {
-	int arr[] = { 27,15,19,18,28,34,65,49,25,37 };
+	//1.建堆 -- 用a中前k个元素建堆
+	int* kMinHeap = (int*)malloc(sizeof(int) * k);
+	assert(kMinHeap);
 
-	for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+	//将a中前k个元素拷贝到kMinHeap中
+	for (int i = 0; i < k; i++)
 	{
-		printf("%d ", arr[i]);
+		kMinHeap[i] = a[i];
+	}
+
+	//建堆
+	for (int i = (k - 1 - 1) / 2; i >= 0; i--)
+	{
+		AdjustDown(kMinHeap, k, i);
+	}
+
+	//2.将剩余n-k个元素依次和堆顶元素做比较,不满规则则替换
+	for (int i = k; i < n; i++)
+	{
+		//比较&替换
+		if (a[i] > kMinHeap[0])
+		{
+			kMinHeap[0] = a[i];
+
+			//堆，所以调整
+			AdjustDown(kMinHeap, k, 0);
+		}
+	}
+
+	//倘若需要降/升序，直接调用堆排序即可
+	//......
+
+	//替换完成，打印
+	for (int i = 0; i < k; i++)
+	{
+		printf("%d ", kMinHeap[i]);
 	}
 	printf("\n");
+}
 
-	Heapsort(arr, sizeof(arr) / sizeof(arr[0]));
-
-	for(int i = 0;i<sizeof(arr) / sizeof(arr[0]); i++)
+//此处测试最大问题
+void TestTopK()
+{
+	int n = 10000;
+	int* a = (int*)malloc(sizeof(int) * n);
+	srand(time(0));
+	for (int i = 0; i < n; ++i)
 	{
-		printf("%d ", arr[i]);
+		a[i] = rand() % 1000000;
 	}
-	printf("\n");
+	a[5] = 1000000 + 1;
+	a[1231] = 1000000 + 2;
+	a[531] = 1000000 + 3;
+	a[5121] = 1000000 + 4;
+	a[120] = 1000000 + 5;
+	a[99] = 1000000 + 6;
+	a[0] = 1000000 + 7;
+	a[76] = 1000000 + 8;
+	a[423] = 1000000 + 9;
+	a[3144] = 1000000 + 10;
+	PrintTopK(a, n, 10);
 }
 
 int main()
 {
 	//Test();
-	TestHeapSort();
+	//TestHeapSort();
+	TestTopK();
 
 	return 0;
 }
