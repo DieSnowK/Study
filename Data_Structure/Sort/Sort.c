d0c1b01dc5f9cb5ed3834b1e8fc5bba94289e6ff
DieSnowK <w1752351098sl@outlook.com>
2023-07-28 00:26:12 +0800

    7.28

---
+++ Data_Structure/Sort/Sort.c
@@ -0,0 +1,36 @@
+#include "Sort.h"
+
+void InsertSort(int* a, int n)
+{
+
+}
+
+void ShellSort(int* a, int n)
+{
+
+}
+
+void SelectSort(int* a, int n)
+{
+
+}
+
+void HeapSort(int* a, int n)
+{
+
+}
+
+void BubbleSort(int* a, int n)
+{
+
+}
+
+void QuickSort(int* a, int n)
+{
+
+}
+
+void MergeSort(int* a, int n)
+{
+
+}
---
+++ Data_Structure/Sort/Sort.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n);
+void SelectSort(int* a, int n);
+void HeapSort(int* a, int n);
+void BubbleSort(int* a, int n);
+void QuickSort(int* a, int n);
+void MergeSort(int* a, int n);
