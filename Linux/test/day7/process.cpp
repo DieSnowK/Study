601263471177d389e581ebd1399e6944675b9d3f
DieSnowK <1752351098@qq.com>
2023-10-06 12:07:53 +0800

    2023-10-6

--- Linux/test/day7/process.cpp
+++ Linux/test/day7/process.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <stdlib.h>
 #include <unistd.h>
 
 int main()
 {
 
 	std::cout << "I am parent process! -- PID:" << getpid() << std::endl;
-	fork();
+	pid_t ret = fork();
 	// 变成两个进程，一个是父进程，一个是子进程
+	if(ret < 0)
+	{
+		perror("fork failed");
+		return 1;
+	}
+	else if (ret > 0)
+	{
+		// parent process
+		std::cout << "parent:" << getpid() << ":is sleeping" << std::endl;
+		sleep(30);
+	}
+	else
+	{
+		// child process
+		std::cout << "child:" << getpid() << ":is sleeping" << std::endl;
+		sleep(5);
+		exit(0);
+	}
 	//while(1)
 	//{
-		std::cout << "Hello SnowK" << std::endl;
-		std::cout << "PID:" << getpid() << " PPID:" << getppid() << std::endl << std::endl;
-		sleep(1);
+	//	std::cout << "Hello SnowK" << std::endl;
+	//	std::cout << "PID:" << getpid() << " PPID:" << getppid() << std::endl << std::endl;
+	//	sleep(1);
 		
 	//}
 	return 0;
