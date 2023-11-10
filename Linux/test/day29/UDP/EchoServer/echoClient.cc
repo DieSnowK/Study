21befb9269f023acf1308685c64b92f670cfb9fa
DieSnowK <1752351098@qq.com>
2023-11-10 22:02:24 +0800

    TODO UDP_ECHO

---
+++ Linux/test/day29/UDP/EchoServer/echoClient.cc
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SIZE 1024
+
+void Usage(std::string proc)
+{
+    std::cout << "\nUsage：" << proc << " ip port" << std::endl;
+}
+
+int main(int argc, char *argv[])
+{
+    if(argc != 3)
+    {
+        Usage(argv[0]);
+        exit(1);
+    }
+    
+    int sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if(sock < 0)
+    {
+        std::cerr << "socket error" << std::endl;
+        exit(2);
+    }
+
+    // client要不要bind？-- 要，但是一般client不会显式bind，程序员不会自己bind
+    // client是一个客户端 --> 普通人下载安装启动使用的 --> 如果程序员自己bind了
+    // --> client一旦bind了一个固定的ip和port，万一其他的客户端提前占用了这个port该怎么办？
+    // client一般不需要显式bind指定port，而是让OS自动随机选择
+    std::string msg;
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof server);
+    server.sin_family = AF_INET;
+    server.sin_port = atoi(argv[2]);
+    server.sin_addr.s_addr = inet_addr(argv[1]);
+
+    char buffer[SIZE];
+    while (true)
+    {
+        std::cout << "Enter your msg# ";
+        std::getline(std::cin, msg);
+
+        // 当client首次发送消息给server的时候，OS会自动给client bind它的IP和PORT
+        sendto(sock, msg.c_str(), msg.size(), 0, (struct sockaddr *)&server, sizeof server);
+
+        struct sockaddr_in tmp;
+        memset(&tmp, 0, sizeof tmp);
+        socklen_t len = sizeof tmp;
+
+        ssize_t s = recvfrom(sock, buffer, sizeof buffer, 0, (struct sockaddr *)&tmp, &len);
+        if(s > 0)
+        {
+            buffer[s] = 0;
+            std::cout << "Server echo# " << buffer << std::endl;
+        }
+    }
+
+    return 0;
+}
---
+++ Linux/test/day29/UDP/EchoServer/echoServer.hpp
@@ -0,0 +1,105 @@
+// echoServer：Client给服务器发送消息，原封不动返回
+#pragma once
+
+#include "Log.hpp"
+#include "Thread.hpp"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define SIZE 1024
+
+class UdpServer
+{
+public:
+    UdpServer(uint16_t port, std::string ip = "")
+    : _port(port), _ip(ip), _sock(-1)
+    {}
+
+    ~UdpServer()
+    {
+        if(_sock >= 0)
+        {
+            close(_sock);
+        }
+    }
+
+    void InitServer()
+    {
+        // 1.创建套接字
+        _sock = socket(AF_INET, SOCK_DGRAM, 0);
+        if(_sock < 0)
+        {
+            LogMessage(FATAL, "%d:%s", errno, strerror(errno));
+            exit(2);
+        }
+
+        // 2.bind：将用户设置的ip和port在内核中和当前的进程强关联
+        struct sockaddr_in local;
+        memset(&local, 0, sizeof local);
+        local.sin_family = AF_INET;
+
+        // 服务器的IP和端口未来也是要发给对方主机的 -->  先要将数据发送到网络
+        local.sin_port = htons(_port);
+
+        // 1. 将点分十进制IP转换为4字节
+        // 2. 4字节主机序列 --> 网络序列
+        // 有一套接口，可以一次性做完这两件事情
+        // INADDR_ANY：让服务器在工作过程中，可以从任意IP中获取数据
+        local.sin_addr.s_addr = _ip.empty() ? INADDR_ANY : inet_addr(_ip.c_str());
+
+        if(bind(_sock, (struct sockaddr*)&local, sizeof local) < 0)
+        {
+            LogMessage(FATAL, "%d:%s", errno, strerror(errno));
+            exit(3);
+        }
+
+        LogMessage(NORMAL, "Init UDP server done... %s", strerror(errno));
+    }
+
+    void Start()
+    {
+        // 作为一款服务器，永远不推出
+        // 服务器启动 --> 进程 --> 常驻进程 --> 永远在内存中存在，除非挂了
+        char buffer[SIZE];
+        while (true)
+        {
+            // peer：纯输出型参数
+            struct sockaddr_in peer;
+            bzero(&peer, sizeof peer); // 将前n个bit位置零，这里功能类似memset，单纯用用，无他
+
+            // start.读取数据
+            // len：输入输出型参数
+            // 输入：peer缓冲区大小
+            // 输出：实际读到的peer
+            socklen_t len = sizeof peer; // 不可设为0，原因如上
+            ssize_t s = recvfrom(_sock, buffer, sizeof(buffer) - 1, 0, (struct sockaddr *)&peer, &len); // UDP协议下，接收消息接口
+            if (s > 0)
+            {
+                buffer[s] = 0;
+                // 这里暂定执行以下任务
+                // 1.输出发送的数据信息
+                // 2.是谁发送的？
+                uint16_t cli_port = ntohs(peer.sin_port); // 数据从网络中来，所以要转
+                std::string cli_ip = inet_ntoa(peer.sin_addr); // 4字节的网络序列IP --> 本主机的字符串风格的IP
+                printf("[%s:%d]# %s\n", cli_ip.c_str(), cli_port, buffer);
+            }
+
+            // 分析和处理数据，TODO
+            // end.写回数据
+            sendto(_sock, buffer, strlen(buffer), 0, (struct sockaddr *)&peer, len);
+        }
+    }
+
+private:
+    // 一个服务器，一般必须需要ip地址和port(16位整数)
+    uint16_t _port;
+    std::string _ip;
+    int _sock;
+};
