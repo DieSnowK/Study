f12fcc0eb69a2bb8034742532bd17436b0fa3d40
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-14 19:12:36 +0800

    Proxy

---
+++ C++/Design_Pattern/Proxy/proxy.cpp
@@ -0,0 +1,33 @@
+class ISubject
+{
+public:
+    virtual void process() = 0;
+};
+
+// Proxy的设计
+class SubjectProxy: public ISubject
+{
+public:
+    virtual void process()
+    {
+        // 对RealSubject的一种间接访问
+        // ...
+    }
+};
+
+class ClientApp
+{
+    ISubject* subject;
+public:
+    ClientApp()
+    {
+        subject = new SubjectProxy();
+    }
+    
+    void DoTask()
+    {
+        // ...
+        subject->process();
+        // ...
+    }
+};
