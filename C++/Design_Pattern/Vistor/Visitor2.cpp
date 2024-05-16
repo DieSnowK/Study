796b99e877a6398de7bc1040f1f574d0810b9458
Wang Shenglong <81177009+DieSnowK@users.noreply.github.com>
2024-05-16 18:09:12 +0800

    Visitor

---
+++ C++/Design_Pattern/Vistor/Visitor2.cpp
@@ -0,0 +1,32 @@
+class Visitor;
+
+class Element
+{
+public:
+    virtual void Func1() = 0;
+    virtual void Func2(int data) = 0;
+    virtual void Func3(int data) = 0;
+    //...
+    
+    virtual ~Element(){}
+};
+
+class ElementA : public Element
+{
+public:
+    void Func1() override
+    {}
+    
+    void Func2(int data) override
+    {}
+};
+
+class ElementB : public Element
+{
+public:
+    void Func1() override
+    {}
+    
+    void Func2(int data) override 
+    {}
+};
