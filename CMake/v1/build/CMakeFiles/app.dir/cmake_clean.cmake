c2df75232c80bd194c894dff3636ac53fc4f8505
DieSnowK <1752351098@qq.com>
2024-06-03 18:06:52 +0800

    CMAKE V1 DONE

---
+++ CMake/v1/build/CMakeFiles/app.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "/home/snowk/repository/Study/CMake/v1/bin/app"
+  "/home/snowk/repository/Study/CMake/v1/bin/app.pdb"
+  "CMakeFiles/app.dir/src/add.c.o"
+  "CMakeFiles/app.dir/src/add.c.o.d"
+  "CMakeFiles/app.dir/src/div.c.o"
+  "CMakeFiles/app.dir/src/div.c.o.d"
+  "CMakeFiles/app.dir/src/main.c.o"
+  "CMakeFiles/app.dir/src/main.c.o.d"
+  "CMakeFiles/app.dir/src/mult.c.o"
+  "CMakeFiles/app.dir/src/mult.c.o.d"
+  "CMakeFiles/app.dir/src/sub.c.o"
+  "CMakeFiles/app.dir/src/sub.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
