1cecf26ee2d3cb56e980e54e8cc117f89f04c814
DieSnowK <1752351098@qq.com>
2024-09-03 19:31:44 +0800

    TestExpireAndTTL && TestType

--- Redis/Demo2/main.cc
+++ Redis/Demo2/main.cc
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <string>
+#include <chrono>
+#include <thread>
 #include <sw/redis++/redis++.h>
+
+#include "utils.hpp"
+
 using namespace std;
 
 void TestGetAndSet(sw::redis::Redis& redis)
@@ -77,12 +82,74 @@ void TestDel(sw::redis::Redis &redis)
     cout << ret << endl;
 }
 
+void TestKeys(sw::redis::Redis &redis)
+{
+    redis.flushall();
+
+    redis.set("key1", "111");
+    redis.set("key2", "222");
+    redis.set("key3", "333");
+    redis.set("key4", "444");
+    redis.set("key5", "555");
+    redis.set("key6", "666");
+
+    // keys 的第二个参数, 是一个 "插入迭代器". 需要先准备好一个保存结果的容器.
+    // 接下来再创建一个插入迭代器指向容器的位置. 就可以把 keys 获取到的结果依次通过刚才的插入迭代器插入到容器的指定位置中了.
+    // 插入迭代器 -> 位置 + 动作
+    vector<string> ret;
+    auto it = std::back_inserter(ret);
+    redis.keys("*", it);
+    PrintContainer(ret);
+}
+
+void TestExpireAndTTL(sw::redis::Redis &redis)
+{
+    redis.flushall();
+
+    redis.set("key", "111");
+    // 10s -> std::chrono::seconds(10);
+    redis.expire("key", 10s);
+
+    std::this_thread::sleep_for(3s);
+
+    auto time = redis.ttl("key");
+    cout << time << endl;
+}
+
+void TestType(sw::redis::Redis& redis)
+{
+    redis.flushall();
+
+    redis.set("key1", "111");
+    string result = redis.type("key");
+    cout << "key1: " << result << endl;
+
+    redis.lpush("key2", "111");
+    result = redis.type("key2");
+    cout << "key2: " << result << endl;
+
+    redis.hset("key3", "aaa", "111");
+    result = redis.type("key3");
+    cout << "key3: " << result << endl;
+
+    redis.sadd("key4", "aaa");
+    result = redis.type("key4");
+    cout << "key4: " << result << endl;
+
+    redis.zadd("key5", "吕布", 99);
+    result = redis.type("key5");
+    cout << "key5: " << result << endl;
+}
+
 int main()
 {
     sw::redis::Redis redis("tcp://127.0.0.1:6379");
     // TestGetAndSet(redis);
     // TestExists(redis);
-    TestDel(redis);
+    // TestDel(redis);
+    // TestKeys(redis);
+    // TestExpireAndTTL(redis);
+    TestType(redis);
 
     return 0;
 }
